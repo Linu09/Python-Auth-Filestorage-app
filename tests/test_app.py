3c4b2bce989faca5d9448b011777f58aaea60f14
Linu Joseph <38865727+Linu09@users.noreply.github.com>
2023-12-06 19:58:15 +0400

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,19 +11,19 @@ class TestYourApplication(unittest.TestCase):
 
     def test_login_route_successful_login(self):
         # Mock the check_user_exists function to simulate a successful login
-        with patch('your_application_file.check_user_exists', return_value=True):
+        with patch('app.check_user_exists', return_value=True):
             response = self.app.post('/login', data={'user_id': 'some_id', 'email': 'test@example.com', 'password': 'password'})
             self.assertEqual(response.status_code, 302)  # Redirects to profile
 
     def test_signup_route_duplicate_user(self):
         # Mock the check_user_exists function to simulate a duplicate user during signup
-        with patch('your_application_file.check_user_exists', return_value=True):
+        with patch('app.check_user_exists', return_value=True):
             response = self.app.post('/signup', data={'user_id': 'some_id', 'email': 'test@example.com', 'password': 'password'})
             self.assertIn(b'User ID or email already exists', response.data)
 
     def test_profile_route_file_upload(self):
         # Mock the upload_file_to_storage function to simulate a successful file upload
-        with patch('your_application_file.upload_file_to_storage', return_value=None):
+        with patch('app.upload_file_to_storage', return_value=None):
             # Assuming the user is already logged in (you can mock the session data)
             response = self.app.post('/profile', data={'file': MagicMock(filename='test.txt')}, content_type='multipart/form-data')
             self.assertIn(b'File uploaded successfully', response.data)
